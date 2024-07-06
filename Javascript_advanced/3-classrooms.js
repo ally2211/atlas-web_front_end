ca94ef19a6f1782047c6c6a42f32b51305075013
ally2211 <amendieta.va@gmail.com>
2024-07-06 14:49:42 -0500

    Number

--- Javascript_advanced/3-classrooms.js
+++ Javascript_advanced/3-classrooms.js
@@ -1,11 +1,11 @@
 function createClassRoom(numbersOfStudents ){
     function studentSeat(seat){
-        return seat;
+        return Number(seat);
     }
     let students = []; 
     for (let i = 0; i < numbersOfStudents; i++)
         students[i] = studentSeat(i + 1);
-    return students[i];
+    return students;
 }
 
 let classRoom = createClassRoom(10);
