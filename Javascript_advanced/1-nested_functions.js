8b7b96b3ad116b1ab493341189b30fcb42fd83fd
ally2211 <amendieta.va@gmail.com>
2024-07-06 13:02:55 -0500

    alert welcome atlas!

---
+++ Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,15 @@
+let globalVariable = "Welcome";
+function outer() {
+    alert(`${globalVariable}`);
+    let course = 'Atlas'
+    function inner(){
+        alert (`${globalVariable} ${course}`);
+        let exclamation = "!";
+        function inception(){
+            alert (`${globalVariable} ${course} ${exclamation}`);
+        }
+        inception();
+    }
+    inner();
+}
+outer();
