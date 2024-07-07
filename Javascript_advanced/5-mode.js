9d88d0f0ef0eda3fca51157cb55a5c26c874fb79
ally2211 <amendieta.va@gmail.com>
2024-07-07 16:57:37 -0500

    DOMContentLoaded

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -1,3 +1,4 @@
+
 //size, weight, transform, background,
 function changeMode(size, weight, transform, background,color){
     return function(){
@@ -8,6 +9,36 @@ function changeMode(size, weight, transform, background,color){
         document.body.style.color = `${color}`
     }
 }
-document.getElementById('Spooky').onclick = changeMode('9', 'bold', 'uppercase', 'pink', 'green')
-document.getElementById('DarkMode').onclick = changeMode('12', 'bold','capitalize','black', 'white')
-document.getElementById('ScreamMode').onclick = changeMode('12', 'normal', 'lowercase', 'white', 'black')
+function main(){
+
+    document.addEventListener('DOMContentLoaded', (event) => {
+        const newParagraph = document.createElement('p');
+        newParagraph.textContent = 'Welcome Atlass!';
+        document.body.appendChild(newParagraph);
+
+        const newButton1 = document.createElement('button');
+        newButton1.id = "Spooky";
+        newButton1.textContent = "Spooky";
+        document.body.appendChild(newButton1);
+
+        const newButton2 = document.createElement('button');
+        newButton2.id = "DarkMode";
+        newButton2.textContent = "Dark Mode";
+        document.body.appendChild(newButton2);
+
+        const newButton3 = document.createElement('button');
+        newButton3.id = "ScreamMode";
+        newButton3.textContent = "Scream Mode";
+        document.body.appendChild(newButton3);
+
+        const Spooky = document.getElementById('Spooky').onclick = changeMode('9', 'bold', 'uppercase', 'pink', 'green')
+        const DarkMode = document.getElementById('DarkMode').onclick = changeMode('12', 'bold','capitalize','black', 'white')
+        const ScreamMode = document.getElementById('ScreamMode').onclick = changeMode('12', 'normal', 'lowercase', 'white', 'black')
+    });
+}
+
+main();
+
+//const Spooky = document.getElementById('Spooky');
+//Spooky.addEventListener('click', changeMode('9', 'bold', 'uppercase', 'pink', 'green'));
+
