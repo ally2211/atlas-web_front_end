5643e4c8abfd8541fbd2728cdbd8f7f797161d11
ally2211 <amendieta.va@gmail.com>
2024-07-07 17:22:34 -0500

    transformText

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -4,7 +4,7 @@ function changeMode(size, weight, transform, background,color){
     return function(){
         document.body.style.fontSize = `${size}`
         document.body.style.fontWeight = `${weight}`
-        document.body.style.transform = `${transform}`
+        document.body.style.textTransform = `${transform}`
         document.body.style.backgroundColor = `${background}`
         document.body.style.color = `${color}`
     }
