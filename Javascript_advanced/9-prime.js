bf6c97351cf85ab49a366656c9c04df1b0e6a9b6
ally2211 <amendieta.va@gmail.com>
2024-07-07 20:48:32 -0500

    calculate execution Time

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,30 @@
+const { endAt } = require("firebase/database");
+
+function countPrimeNumbers(limit) {
+
+    const isPrime = new Array(limit + 1).fill(true);
+    isPrime[0] = isPrime[1] = false;
+
+    for (let i=2; i * i <= limit; i++) {
+        if (isPrime[i]) {
+            for (let j=1 * i; j<= limit; j+= i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    let primeCount= 0;
+    for (let i = 2; i<= limit; i++) {
+        if (isPrime[i]) {
+            primeCount++;
+        }
+    }
+    return primeCount;
+  }
+ 
+const limit = 100;
+const startTime = performance.now();
+const primeCount = countPrimeNumbers(limit);
+const endTime = performance.now();
+const executionTime = endTime - startTime;
+console.log(`Execution time of printing countPrimeNumbers was ${executionTime} milliseconds.`)
