8a1444cdb5019775bbe559a14420bc5ff722f072
ally2211 <amendieta.va@gmail.com>
2024-07-08 00:13:37 -0500

    add alert no fetching

--- Javascript_advanced/14-wikipedia.js
+++ Javascript_advanced/14-wikipedia.js
@@ -10,18 +10,18 @@ function queryWikipedia(callback) {
     const url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*';
 
     xhr.onreadystatechange = function() {
-        if (xhr.status ===4){
+        if (xhr.status === 4){
             if (xhr.status === 200) {
                 const response = JSON.parse(xhr.responseText);
                 const extractText = response.extract;
-                alert(extractText);
+                alert(`extracted text: ${extractText}`);
                 callback(extractText);
             }
-        }
         else {
             console.error('Error fetching data:', xhr.statusText);
+            }
         }
-    }
+    };
     xhr.open('GET', url,true);
     xhr.send();
 }
